f441a2dd2f95f8becac5a49a954b3c0fd7b3c54b
lukmanasaputra <squadwts2@gmail.com>
2025-05-26 03:04:23 +0700

    test

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const db = require("./App/database");
 const mainRouter = require("./App/routes/main.js");
 
 const app = express();
+console.log(path.join(__dirname, "./App/views"));
 
 const port = process.env.PORT || 8080;
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("layout", "layouts/layout");
-app.set("views", path.join(__dirname, "../App/views"));
-app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "./App/views"));
+app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
